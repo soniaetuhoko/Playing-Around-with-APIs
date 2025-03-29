7e0159e14c2e28bb0a32010278264615de487857
soniaetuhoko <s.etuhoko@alustudent.com>
2025-03-29 22:09:30 +0200

    added

---
+++ js/env.js
@@ -0,0 +1,8 @@
+/**
+ * Environment variables configuration
+ * This file should be loaded before other scripts
+ */
+
+const ENV = {
+    SPOONACULAR_API_KEY: 'b5655c544b644a18a309fb2fe5be8d5e' // Replace this with your actual API key
+};
